4b687b8e3a48dc4b9115ecd0ba3a88cfdbbf290b
gadipura <galang8280@students.amikom.ac.id>
2020-10-05 14:21:41 +0700

    best-practice added to vowel count kata

--- CodeWars - kata/7-kyu/Vowel Count/index.js
+++ CodeWars - kata/7-kyu/Vowel Count/index.js
@@ -16,6 +16,26 @@
 //solution
 const getCount = str => str.split('').reduce((vowelsCount, str) => /[aiueo]/g.test(str) ? vowelsCount += 1 : vowelsCount, 0)
 
+//best-practice(long)
+// function getCount(str) {
+//     var vowelsCount = 0;
+//     var vowels = ["a","e","i","o","u"];
+//     for(var i = 0;i < str.length;i++){
+//       for(var j=0;j<vowels.length;j++){
+//         if(str[i] === vowels[j]){
+//           vowelsCount++;
+//         }
+//       }
+//     }
+
+//     return vowelsCount;
+//   }
+
+//best-practice(short)
+// function getCount(str) {
+//     return (str.match(/[aeiou]/ig)||[]).length;
+//   }
+
 //sample-test
 // describe("Case 1", function(){
 //     it ("should be defined", function(){
