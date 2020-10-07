5d27d7d6efbe881bccde390cebc4c560ea16624d
gadipura <galang8280@students.amikom.ac.id>
2020-10-07 14:42:08 +0700

    add new kata 8-kyu Geneerate Range of Int

---
+++ CodeWars - kata/8-kyu/Generate Range of Integer/index.js
@@ -0,0 +1,33 @@
+/*
+
+    Title:
+        Generate Range of Integer
+
+    Description
+        Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step.
+        The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+    Task
+        Implement a function named
+
+        generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+        generateRange(1, 10, 3) // should return array of [1,4,7,10]
+    
+    Note
+        min < max
+        step > 0
+        the range does not HAVE to include max (depending on the step)
+
+*/
+
+//Solution
+function generateRange(min, max, step) {
+    const element = [];
+    if (min < max) {
+        for (let i = min; i <= max; i += step) {
+            element.push(i)
+        }
+    }
+    return element;
+}
+console.log(generateRange(2, 10, 2), [2, 4, 6, 8, 10]);
